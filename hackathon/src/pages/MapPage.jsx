c8b6821e31b18541964fd526081f3f4f5e128a8d
Petro <petrobida612@gmail.com>
2026-04-04 09:10:33 +0300

    integrated a basic postcard map view

---
+++ hackathon/src/pages/MapPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const MapPage = () => {
+  return (
+    <div className="page-content">
+      <h1>Global Radar</h1>
+      <div className="card map-placeholder">
+        <p>Leaflet map integration pending...</p>
+      </div>
+    </div>
+  );
+};
+
+export default MapPage;
